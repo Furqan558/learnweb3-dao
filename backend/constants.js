59c986d1e221f734c2a533afae6b9df3162c5a47
Furqan558 <furqanhameed558@gmail.com>
2023-05-10 14:01:31 +0500

    DAO for my NFT Holders

---
+++ backend/constants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+	"0xca15Cd6d938b58A581e9d0bbB5d8B41aC51262C7";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
